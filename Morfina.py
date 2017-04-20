eba47380871104a0d0602f907fc9142d362a4d4b
DualStream799 <dualstream799@gmail.com>
2017-04-20 12:40:08 -0300

    Correções de texto

--- Morfina.py
+++ Morfina.py
@@ -1,17 +1,17 @@
 from IMC_Calculator import Calcular_IMC()
+"""
+Ms: Quantidade de morfina absorvida no sangue
+m:  Quantidade de Morfina
+c:  Capacidade a capacidade máxima do corpo de excretar morfina
+f:  Função da quantidade de morfina na injeção/via oral: Relaciona massa(w)/altura(h) -(IMC)- a quantidade de morfina
+g:  Função de excreção de morfina: Relaciona a capacidade máxima do corpo de excretar morfina e a a quantidade de morfina no sangue (fator limitante)
 
-# Ms: Quantidade de morfina absorvida no sangue
-# m:  Quantidade de Morfina
-# c:  Capacidade a capacidade máxima do corpo de excretar morfina
-# f:  Função da quantidade de morfina na injeção/via oral: Relaciona massa(w)/altura(h) -(IMC)- a quantidade de morfina
-# g:  Função de excreção de morfina: Relaciona a capacidade máxima do corpo de excretar morfina e a a quantidade de morfina no sangue (fator limitante)
-
-#ESTOQUES E FLUXOS (EQUÇÕES À DIFRENÇAS):
-# Ms(t+1) = Ms(t) + f(h,w,m) - g(c,Ms)
+ESTOQUES E FLUXOS (EQUÇÕES À DIFRENÇAS):
+ Ms(t+1) = Ms(t) + f(h,w,m) - g(c,Ms)
+"""
 
 
-
-'''
+"""
 Via oral: 0,2 a 0,5 (0,3500) mg/kg, a cada 4 a 24 horas.
 
 Injetável:
@@ -29,5 +29,5 @@ Injetável:
     #manutenção: 0,1 a 0,2  (0,1500) mg/kg/dose, a cada 2 e 4 horas;
  
     #dose máxima: 15 mg/dose; infusão contínua: 0,025 a 0,1 (0,0625 ) mg/kg/hora.
-'''
+"""
 
