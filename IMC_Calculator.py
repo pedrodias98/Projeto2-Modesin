4f95dfc4143cabf404b4497605aa91e951da87a5
DualStream799 <dualstream799@gmail.com>
2017-04-20 11:45:56 -0300

    Upload de IMC_Calculator.py

--- IMC_Calculator.py
+++ IMC_Calculator.py
@@ -4,5 +4,19 @@ Created on Thu Mar 20 10:34:57 2017
 
 @author: dualstream799
 """
-
-i1 = 
+# Criando função para calcular o IMC:
+def Calcular_IMC():
+# Criando as entradas:
+    w = float(input("Insira o seu peso (em metros): "))        #w -> weight(peso)
+    h = float(input("Insira a sua altura (em quilogramas):"))  #h -> height(altura)
+# Calculando o IMC:
+    IMC = (1.3*w)/((h)**2.5)
+# Gerando o cálculo:
+    if IMC < 18.5:
+        print("Você está abaixo do peso")
+    elif IMC >= 18.5 and IMC <= 24.9:
+        print("Você está no peso ideal")
+    elif IMC >= 25 and IMC <= 29.9:
+        print("Você está com sobrepeso")
+    else:
+        print("Você está obeso")
